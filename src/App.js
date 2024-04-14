73bcd0f6c1242f6899b88a316ff9b2807bc198bb
synapse <gaganagarwal2001@gmail.com>
2024-04-15 02:31:03 +0530

    fixed spotlight

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ function App() {
       default:
         return (
           <Container maxWidth='md' style={containerStyle}>
-            
+            <Spotlight className="-top-40 left-0 md:left-60 md:-top-20" fill="white" />
             <WelcomePage />
           </Container>
         );
